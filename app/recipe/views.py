86223e061da689abdf67876fac445dc5585ae367
Sergio Valero García <sergiovalero92@gmail.com>
2021-10-29 11:59:08 +0200

    Added feature to filter recipes

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -48,9 +48,25 @@ class RecipeViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.RecipeSerializer
     queryset = Recipe.objects.all()
 
+    def _params_to_ints(self, qs):
+        """Convert a list of strings IDs to a list of integers"""
+        return [int(str_id) for str_id in qs.split(",")]
+
     def get_queryset(self):
         """Return objects for the currect authenticated user only"""
-        return self.queryset.filter(user=self.request.user).order_by("-id")
+        tags = self.request.query_params.get("tags")
+        ingredients = self.request.query_params.get("ingredients")
+        queryset = self.queryset
+
+        if tags:
+            tag_ids = self._params_to_ints(tags)
+            queryset = queryset.filter(tags__id__in=tag_ids)
+
+        if ingredients:
+            ingredient_ids = self._params_to_ints(ingredients)
+            queryset = queryset.filter(ingredients__id__in=ingredient_ids)
+
+        return queryset.filter(user=self.request.user).order_by("-id")
 
     def get_serializer_class(self):
         """Return appropiate serializer class"""
